021a5ae12b9802ce6ff70436df66fb4be87695b0
Dumbie <wasleyron668@gmail.com>
2025-01-12 14:34:58 +0500

    added extra files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { ToastContainer } from 'react-toastify'
 import Login from './pages/Login'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Faq from './pages/Faq'
+import Products from './pages/Products'
+import Wishes from './pages/wishes'
 
 function App() {
 	return (
 		<div>
 			<ToastContainer />
 			<Login />
+			<Routes>
+				<Route path='/' element={<App />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/cart' element={<Cart />} />
+				<Route path='/faq' element={<Faq />} />
+				<Route path='/products' element={<Products />} />
+				<Route path='/wishes' element={<Wishes />} />
+			</Routes>
 		</div>
 	)
 }
