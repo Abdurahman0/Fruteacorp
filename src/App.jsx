69454ef1aa2f422361cd7510d980ec9ace72a6d8
Dumbie <wasleyron668@gmail.com>
2025-01-12 14:40:59 +0500

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { ToastContainer } from 'react-toastify'
-import Login from './pages/Login'
 import { Route, Routes } from 'react-router-dom'
 import About from './pages/About'
 import Cart from './pages/Cart'
 import Faq from './pages/Faq'
 import Products from './pages/Products'
 import Wishes from './pages/wishes'
+import Main from './pages/Main'
 
 function App() {
 	return (
 		<div>
 			<ToastContainer />
-			<Login />
 			<Routes>
-				<Route path='/' element={<App />} />
+				<Route path='/' element={<Main />} />
 				<Route path='/about' element={<About />} />
 				<Route path='/cart' element={<Cart />} />
 				<Route path='/faq' element={<Faq />} />
