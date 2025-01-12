7a0e69e12f8e08c8e346972d6ea2a89698e475a3
rasuljon313 <anvarovrasuljon313@gmail.com>
2025-01-12 22:45:16 +0500

    endi toliq mainga otdim

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 function Login() {
 	const [view, setView] = useState('login') // Tracks the current view: 'login', 'signup', 'forgot'
-	const navigate = useNavigate()
 	const [formData, setFormData] = useState({
 		firstName: '',
 		lastName: '',
@@ -56,7 +54,6 @@ function Login() {
 					localStorage.setItem('refreshToken', RefreshToken)
 					setIsAuthenticated(true) // Update state to hide the component
 					toast('Request successful!')
-					navigate("/about")
 				} else {
 					toast('No token received')
 				}
