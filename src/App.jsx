21c4b43b362c90f5387bb1569488b4a0404fafd5
Dumbie <wasleyron668@gmail.com>
2025-01-14 10:38:07 +0500

    fixed wishes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import About from './pages/About'
 import Cart from './pages/Cart'
 import Faq from './pages/Faq'
 import Products from './pages/Products'
-import Wishes from './pages/wishes'
 import Main from './pages/Main'
+import Wishes from './pages/Wishes'
 
 function App() {
 	return (
 		<div>
 			<ToastContainer />
 			<Routes>
-			    <Route path='/' element={<Main />}/>
+				<Route path='/' element={<Main />} />
 				<Route path='/about' element={<About />} />
 				<Route path='/cart' element={<Cart />} />
 				<Route path='/faq' element={<Faq />} />
