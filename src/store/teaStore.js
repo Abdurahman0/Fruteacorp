f0606a73115568e105a0f5d2a21d2f55f0d8e5f9
Dumbie <wasleyron668@gmail.com>
2025-01-17 23:21:41 +0500

    finished main page

--- src/store/teaStore.js
+++ src/store/teaStore.js
@@ -1,24 +1,15 @@
 import { create } from 'zustand'
 
 const useStore = create(set => ({
-<<<<<<< HEAD
-	
-	isSidebarActive: true, 
+	isSidebarActive: true,
 	toggleSidebar: () =>
 		set(state => ({ isSidebarActive: !state.isSidebarActive })),
 
-=======
-	isSidebarActive: true, 
-	toggleSidebar: () =>
-		set(state => ({ isSidebarActive: !state.isSidebarActive })),
->>>>>>> remotes/origin/rasuljon
-	
 	user: null,
 	setUser: user => set({ user }),
 
-	languagee: "uz",
-	setLanguage: (i) => set({ languagee: i }),
-
+	languagee: 'uz',
+	setLanguage: i => set({ languagee: i }),
 }))
 
 export default useStore
