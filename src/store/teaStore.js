beeebd28676473ee323a774d71ccc291d81e234c
Dumbie <wasleyron668@gmail.com>
2025-01-19 15:15:59 +0500

    added user to zustand

--- src/store/teaStore.js
+++ src/store/teaStore.js
@@ -5,8 +5,11 @@ const useStore = create(set => ({
 	toggleSidebar: () =>
 		set(state => ({ isSidebarActive: !state.isSidebarActive })),
 
-	user: null,
-	setUser: user => set({ user }),
+	user: JSON.parse(localStorage.getItem('user')) || null, // Load user from localStorage
+	setUser: user => {
+		set({ user })
+		localStorage.setItem('user', JSON.stringify(user)) // Save user to localStorage
+	},
 
 	languagee: 'uz',
 	setLanguage: i => set({ languagee: i }),
